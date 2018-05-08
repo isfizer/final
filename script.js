83b5a28b317bfb2a84aed698fe65f5f180d53811
isfizer <35748740+isfizer@users.noreply.github.com>
2018-05-08 17:59:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+     $('.carousel').carousel();
+     $(".dropdown-trigger").dropdown();
+  });
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+
+ $(document).ready(function(){
+    $('.parallax').parallax();
+  });
